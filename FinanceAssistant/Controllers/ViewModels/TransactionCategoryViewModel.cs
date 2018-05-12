ba710f3a7f20d3d2a0b16ab6cae18fa7f3c90d76
TAJANA-PRO\Tajana <tajana.pavlak@gmail.com>
2018-05-12 10:59:22 +0200

    Added ViewModels

---
+++ FinanceAssistant/Controllers/ViewModels/TransactionCategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceAssistant.Controllers.ViewModels
+{
+    public class TransactionCategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public int TypeId { get; set; } // foreign key
+        public TransactionTypeViewModel Type { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ FinanceAssistant/Controllers/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceAssistant.Controllers.ViewModels
+{
+    public class TransactionViewModel
+    {
+        public int Id { get; set; }
+
+        public int TypeId { get; set; } // foreign key
+        public TransactionTypeViewModel Type { get; set; }
+
+        public int CategoryId { get; set; } // foreign key
+        public TransactionCategoryViewModel Category { get; set; }
+
+        public string Description { get; set; }
+
+        public int Amount { get; set; }
+
+        public string Currency { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
