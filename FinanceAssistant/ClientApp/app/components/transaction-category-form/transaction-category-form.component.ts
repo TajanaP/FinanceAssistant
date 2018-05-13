5d2a5b6e3e8bb0a8c0894f80284d9fae896773a5
TAJANA-PRO\Tajana <tajana.pavlak@gmail.com>
2018-05-13 14:11:25 +0200

    Transaction Category - Added Angular Form

---
+++ FinanceAssistant/ClientApp/app/components/transaction-category-form/transaction-category-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-category-form',
+  templateUrl: './transaction-category-form.component.html',
+  styleUrls: ['./transaction-category-form.component.css']
+})
+export class TransactionCategoryFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
